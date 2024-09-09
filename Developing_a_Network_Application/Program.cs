a503193970a9ad8496ede78de0fac6d36d490a29
bak88 <dimbak1990@gmail.com>
2024-09-09 15:07:54 +0300

    first commit

---
+++ Developing_a_Network_Application/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Developing_a_Network_Application
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
