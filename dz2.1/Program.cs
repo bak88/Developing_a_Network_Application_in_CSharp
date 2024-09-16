4cfc3042e9f424c9da9c501ce78fd925b354ca1f
bak88 <dimbak1990@gmail.com>
2024-09-16 23:02:12 +0300

    dz2

---
+++ dz2.1/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace dz2._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            bool isWork = true;
+            string stopWord = "Exit".ToLower();
+            string message;
+
+            while (isWork)
+            {
+                Console.WriteLine("Enter message: ");
+                message = Console.ReadLine()!;
+
+                if (message.ToLower() == stopWord)
+                {
+                    isWork = false;
+                }
+            }
+
+        }
+    }
+
+}
--- dz_1.1/Message.cs
+++ dz_1.1/Message.cs
@@ -1,12 +1,29 @@
-﻿using System;
+﻿using dz_1._1;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace dz_1._1
 {
     internal class Message
     {
+        public string FromName { get; set; }
+        public DateTime Date { get; set; }
+        public string Text { get; set; }
+
+        public string ToJson()
+        {
+            return JsonSerializer.Serialize(this);
+        }
+        // Статический метод для десериализации JSON в объект MyMessage
+        public static Message FromJson(string json)
+        {
+            return JsonSerializer.Deserialize<Message>(json);
+        }
     }
 }
+
+
