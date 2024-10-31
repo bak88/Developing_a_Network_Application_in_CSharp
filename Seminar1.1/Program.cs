4db90a872fdf7ba3507b09f7605d62c921ad4339
bak88 <dimbak1990@gmail.com>
2024-10-31 12:17:59 +0300

    Seminar 1

---
+++ Seminar1.1/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Seminar1._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Chat.Server();
+            }
+            else
+            {
+                Chat.Client(args[0]);
+            }
+        }
+    }
+}
